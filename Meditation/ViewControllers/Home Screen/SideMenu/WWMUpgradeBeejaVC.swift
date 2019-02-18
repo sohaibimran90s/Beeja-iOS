0177537d9e54023dba28b38a20dc5f929672dbac
Roshan Kumawat <roshan.kumawat@Del-LM-79.local>
2019-02-18 18:52:38 +0530

    Add Inapp Puchase and Upgrade beeja screen design

--- Meditation/ViewControllers/Home Screen/SideMenu/WWMUpgradeBeejaVC.swift
+++ Meditation/ViewControllers/Home Screen/SideMenu/WWMUpgradeBeejaVC.swift
@@ -11,16 +11,34 @@ import StoreKit
 
 class WWMUpgradeBeejaVC: WWMBaseViewController,SKProductsRequestDelegate,SKPaymentTransactionObserver {
     
+    @IBOutlet weak var viewLifeTime: UIView!
+    @IBOutlet weak var viewAnnually: UIView!
+    @IBOutlet weak var viewMonthly: UIView!
     
 
+    var selectedProductIndex = 1
     var transactionInProgress = false
     var productsArray = [SKProduct]()
-    var productIDs = [String]()
+    var productIDs = ["get_6_gbp_monthly_sub","get_42_gbp_annual_sub","get_108_gbp_lifetime_sub"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.setNavigationBar(isShow: false, title: "")
+        self.viewAnnually.isHidden = false
+        self.viewLifeTime.isHidden = true
+        self.viewMonthly.isHidden = true
+        
+        self.viewMonthly.layer.borderWidth = 2.0
+        self.viewLifeTime.layer.borderWidth = 2.0
+        self.viewAnnually.layer.borderWidth = 2.0
+        
+        self.viewMonthly.layer.borderColor = UIColor.init(hexString: "#00eba9")!.cgColor
+        self.viewLifeTime.layer.borderColor = UIColor.init(hexString: "#00eba9")!.cgColor
+        self.viewAnnually.layer.borderColor = UIColor.init(hexString: "#00eba9")!.cgColor
+        
         self.requestProductInfo()
+        SKPaymentQueue.default().add(self)
         // Do any additional setup after loading the view.
     }
 
@@ -42,19 +60,58 @@ class WWMUpgradeBeejaVC: WWMBaseViewController,SKProductsRequestDelegate,SKPayme
         }
     }
     
+    func showActions() {
+        if transactionInProgress {
+            return
+        }
+        
+        let actionSheetController = UIAlertController(title: kAlertTitle, message: "What do you want to do?", preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let buyAction = UIAlertAction(title: "Buy", style: UIAlertAction.Style.default) { (action) -> Void in
+            let payment = SKPayment(product: self.productsArray[self.selectedProductIndex] )
+            SKPaymentQueue.default().add(payment)
+            self.transactionInProgress = true
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) { (action) -> Void in
+            
+        }
+        
+        actionSheetController.addAction(buyAction)
+        actionSheetController.addAction(cancelAction)
+        
+        present(actionSheetController, animated: true, completion: nil)
+    }
+    
     // MARK:- UIButton Action
     
     
     @IBAction func btnMonthlyAction(sender: AnyObject) {
-        
+        self.viewAnnually.isHidden = true
+        self.viewLifeTime.isHidden = true
+        self.viewMonthly.isHidden = false
+        self.selectedProductIndex = 0
     }
     
     @IBAction func btnAnnuallyAction(sender: AnyObject) {
+        self.viewAnnually.isHidden = false
+        self.viewLifeTime.isHidden = true
+        self.viewMonthly.isHidden = true
+        self.selectedProductIndex = 1
         
     }
     
     @IBAction func btnLifeTimeAction(sender: AnyObject) {
+        self.viewAnnually.isHidden = true
+        self.viewLifeTime.isHidden = false
+        self.viewMonthly.isHidden = true
+        self.selectedProductIndex = 2
         
+    }
+    
+    @IBAction func btnContinuePaymentAction(sender: AnyObject) {
+        
+        self.showActions()
     }
     
     
