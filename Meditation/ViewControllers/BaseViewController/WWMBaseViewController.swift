b8cc68fbc7f40327c3b18cbb44655a03d7c03c09
Prema Negi <prema.negi@Del-LM-92.local>
2019-06-21 11:29:35 +0530

    version update

--- Meditation/ViewControllers/BaseViewController/WWMBaseViewController.swift
+++ Meditation/ViewControllers/BaseViewController/WWMBaseViewController.swift
@@ -184,35 +184,17 @@ class WWMBaseViewController: UIViewController {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMSideMenuVC") as! WWMSideMenuVC
         self.navigationController?.pushViewController(vc, animated: false)
     }
+    
     @IBAction func btnFlightModeAction(_ sender: UIButton) {
-       // let settingsUrl = URL.init(string: UIApplication.openSettingsURLString)
-        //App-prefs:root=AIRPLANE_MODE
         if let url = URL(string:UIApplication.openSettingsURLString) {
             if UIApplication.shared.canOpenURL(url) {
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
             }
         }
-//        else {
-//            if let setUrl = settingsUrl {
-//                UIApplication.shared.open(setUrl, options: [:]) { (Bool) in
-//                    print("Sucess")
-//                }
-//            }
-//
-//        }
     }
+    
     @IBAction func btnBackAction(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
