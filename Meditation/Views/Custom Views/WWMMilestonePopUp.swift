6d5810f53aee95b156c0da83b4186578e403d253
Prema Negi <prema.negi@Del-LM-92.local>
2019-07-03 18:56:06 +0530

    dev-PN1

---
+++ Meditation/Views/Custom Views/WWMMilestonePopUp.swift
@@ -0,0 +1,21 @@
+//
+//  WWMMilestonePopUp.swift
+//  Meditation
+//
+//  Created by Prema Negi on 03/07/19.
+//  Copyright © 2019 Cedita. All rights reserved.
+//
+
+import UIKit
+
+class WWMMilestonePopUp: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
