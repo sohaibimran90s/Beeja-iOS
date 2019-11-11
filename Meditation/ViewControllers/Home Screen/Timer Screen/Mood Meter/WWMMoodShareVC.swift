c434f7a757543eb44f33152ed6acea9a2cfd38f9
Prema Negi <prema.negi@FVFVHEMDJ1WK.irisnation.com>
2019-11-11 18:05:36 +0530

    side menu text changes

--- Meditation/ViewControllers/Home Screen/Timer Screen/Mood Meter/WWMMoodShareVC.swift
+++ Meditation/ViewControllers/Home Screen/Timer Screen/Mood Meter/WWMMoodShareVC.swift
@@ -165,6 +165,7 @@ class WWMMoodShareVC: UIViewController,UICollectionViewDelegate,UICollectionView
                 let text = "Feeling \(self.moodData.name) with Beeja. Download the app here: http://itunes.com/apps/com.beejameditation.beeja"
                 let imageToShare = [text,image] as [Any]
                 let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+                activityViewController.setValue("Feeling \(self.moodData.name) with Beeja", forKey: "Subject")
                 
                 activityViewController.completionWithItemsHandler = { (activity, success, items, error) in
                     print(success ? "SUCCESS!" : "FAILURE")
@@ -199,6 +200,7 @@ class WWMMoodShareVC: UIViewController,UICollectionViewDelegate,UICollectionView
                     let text = "Feeling \(self.moodData.name) with Beeja. Download the app here: http://itunes.com/apps/com.beejameditation.beeja"
                     let imageToShare = [text,image] as [Any]
                     let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+                    activityViewController.setValue("Feeling \(self.moodData.name) with Beeja", forKey: "Subject")
                     
                     activityViewController.completionWithItemsHandler = { (activity, success, items, error) in
                         print(success ? "SUCCESS!" : "FAILURE")
