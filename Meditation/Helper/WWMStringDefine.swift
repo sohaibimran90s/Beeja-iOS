fe00776cf97d74c8f9adce20fc57f454dce8643a
Prema Negi <prema.negi@FVFVHEMDJ1WK.irisnation.com>
2019-11-11 19:03:21 +0530

    version update and betaenabled true

--- Meditation/Helper/WWMStringDefine.swift
+++ Meditation/Helper/WWMStringDefine.swift
@@ -311,7 +311,7 @@ let  Validatation_JournalOfflineMsg  =
 
 
 // If kBETA_ENABLED is true its mean we are using beta_url else using staging_url
-let kBETA_ENABLED = false
+let kBETA_ENABLED = true
 
 
 let URL_PrivacyPolicy   =  URL_BASE_WEBVIEW + "/privacy-policy?mobile_view=1"
