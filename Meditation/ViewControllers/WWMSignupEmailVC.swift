9da367eba60180ae05b57c5e5233a10a571ae3a2
prema negi <prema.negi@iris-worldwide.com>
2020-04-22 22:07:24 +0530

    design login changes

--- Meditation/ViewControllers/WWMSignupEmailVC.swift
+++ Meditation/ViewControllers/WWMSignupEmailVC.swift
@@ -9,8 +9,10 @@
 import UIKit
 import IQKeyboardManagerSwift
 import FirebaseCrashlytics
+import GoogleSignIn
+import FBSDKLoginKit
 
-class WWMSignupEmailVC: WWMBaseViewController, UITextFieldDelegate {
+class WWMSignupEmailVC: WWMBaseViewController, UITextFieldDelegate, GIDSignInDelegate, GIDSignInUIDelegate {
     
     @IBOutlet weak var txtViewName: UITextField!
     @IBOutlet weak var viewName: UIView!
@@ -140,91 +142,10 @@ class WWMSignupEmailVC: WWMBaseViewController, UITextFieldDelegate {
             WWMHelperClass.showPopupAlertController(sender: self, message: KENTERPASS, title: kAlertTitle)
         }else{
             if isFromFb {
-                self.loginWithSocial()
+                self.loginWithSocial(param: fbData)
             }else {
                 self.signUpApi()
             }
-        }
-    }
-    
-    func loginWithSocial() {
-        self.view.endEditing(true)
-        //WWMHelperClass.showSVHud()
-        WWMHelperClass.showLoaderAnimate(on: self.view)
-        
-        
-        let param = [
-            "email": txtViewEmail.text ?? "",
-            "password":"",
-            "deviceId": kDeviceID,
-            "deviceToken" : appPreference.getDeviceToken(),
-            "DeviceType": kDeviceType,
-            "loginType": kLoginTypeFacebook,
-            "profile_image":"http://graph.facebook.com/\(fbData["id"] ?? "")/picture?type=large",
-            "socialId":"\(fbData["id"] ?? -1)",
-            "name":"\(fbData["name"] ?? "")",
-            "model": UIDevice.current.model,
-            "version": UIDevice.current.systemVersion
-            ] as [String : Any]
-        
-        print("param2... \(param)")
-        WWMWebServices.requestAPIWithBody(param:param , urlString: URL_LOGIN, context: "WWMSignupEmailVC", headerType: kPOSTHeader, isUserToken: false) { (result, error, sucess) in
-            if sucess {
-                
-                if let userProfile = result["userprofile"] as? [String:Any] {
-                    
-                    print("userProfile WWMSignupEmailVC... \(userProfile)")
-                    
-                    if let isProfileCompleted = userProfile["IsProfileCompleted"] as? Bool {
-                        self.appPreference.setIsLogin(value: true)
-                    self.appPreference.setUserID(value:"\(userProfile["user_id"] as? Int ?? 0)")
-                        //Crashlytics.sharedInstance().setUserIdentifier("userId \(userProfile["user_id"] as? Int ?? 0)")
-                        
-                        Crashlytics.crashlytics().setUserID("userId \(userProfile["user_id"] as? Int ?? 0)")
-                        
-                        //NotificationCenter.default.post(name: Notification.Name(rawValue: "logoutSuccessful"), object: nil)
-                        
-                        self.appPreference.setEmail(value: userProfile["email"] as? String ?? "")
-                        self.appPreference.setUserToken(value: userProfile["token"] as? String ?? "")
-                        self.appPreference.setUserName(value: userProfile["name"] as? String ?? "")
-                        self.appPreference.setIsProfileCompleted(value: isProfileCompleted)
-                        self.appPreference.setType(value: userProfile["type"] as? String ?? "")
-                        self.appPreference.setGuideType(value: userProfile["guided_type"] as? String ?? "")
-                        self.appPreference.setGuideTypeFor3DTouch(value: userProfile["guided_type"] as? String ?? "")
-                        self.appPreference.setUserData(value: [:])
-                        
-                        print("self.appPreference.getUserName() ...... \(self.appPreference.getUserName())")
-                        
-                        self.appPreference.setHomePageURL(value: userProfile["home_page_url"] as! String)
-                        self.appPreference.setLearnPageURL(value: userProfile["learn_page_url"] as! String)
-                                                
-                        if isProfileCompleted {
-                            
-                            self.appPreference.setGetProfile(value: true)
-                            
-                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMTabBarVC") as! WWMTabBarVC
-                            UIApplication.shared.keyWindow?.rootViewController = vc
-                            
-                            // UIApplication.shared.keyWindow?.rootViewController = AppDelegate.sharedDelegate().animatedTabBarController()
-                        }else {
-                            
-                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMWalkThoghVC") as! WWMWalkThoghVC
-                            
-                            vc.value = "SignupLetsStart"
-                            self.navigationController?.pushViewController(vc, animated: true)
-                        }
-                    }
-                }
-            }else {
-                if error?.localizedDescription == "The Internet connection appears to be offline."{
-                    WWMHelperClass.showPopupAlertController(sender: self, message: internetConnectionLostMsg, title: kAlertTitle)
-                }else{
-                    WWMHelperClass.showPopupAlertController(sender: self, message: error?.localizedDescription ?? "", title: kAlertTitle)
-                }
-                
-            }
-            //WWMHelperClass.dismissSVHud()
-            WWMHelperClass.hideLoaderAnimate(on: self.view)
         }
     }
     
@@ -277,10 +198,201 @@ class WWMSignupEmailVC: WWMBaseViewController, UITextFieldDelegate {
                     vc.value = "SignupLetsStart"
                     self.navigationController?.pushViewController(vc, animated: true)
                 }else {
+                    GIDSignIn.sharedInstance()?.signOut()
                     WWMHelperClass.showPopupAlertController(sender: self, message:  result["message"] as? String ?? "Unauthorized request", title: kAlertTitle)
                 }
                 
+            }else {
+                GIDSignIn.sharedInstance()?.signOut()
+                if error?.localizedDescription == "The Internet connection appears to be offline."{
+                    WWMHelperClass.showPopupAlertController(sender: self, message: internetConnectionLostMsg, title: kAlertTitle)
+                }else{
+                    WWMHelperClass.showPopupAlertController(sender: self, message: error?.localizedDescription ?? "", title: kAlertTitle)
+                }
                 
+            }
+            //WWMHelperClass.dismissSVHud()
+            WWMHelperClass.hideLoaderAnimate(on: self.view)
+        }
+    }
+    
+    //MARK: Social Login & Terms n Condition button
+    
+    @IBAction func btnLoginWithFacebookAction(_ sender: UIButton) {
+           WWMHelperClass.sendEventAnalytics(contentType: "SIGN_IN", itemId: "FACEBOOK", itemName: "")
+           let loginManager = LoginManager()
+           loginManager.logIn(permissions: ["public_profile", "email"], from: self) { (loginResult, error) in
+
+               if error != nil {
+                   print(error?.localizedDescription ?? "")
+               }else {
+                   if (loginResult?.isCancelled)! {
+                       print("User Cancellled To login with fb")
+                   }else {
+                       let req = GraphRequest.init(graphPath: "me", parameters: ["fields":"email,name"], tokenString:loginResult?.token?.tokenString , version: nil, httpMethod: HTTPMethod(rawValue: "GET"))
+                       req.start(completionHandler: { (connection, result, error) in
+                       if error == nil {
+                           print(result!)
+                           if let fbData = result as? [String:Any] {
+                               if let fbEmail = fbData["email"] as? String {
+
+                                   let param = [
+                                       "email": fbEmail,
+                                       "password":"",
+                                       "deviceId": kDeviceID,
+                                       "DeviceType": kDeviceType,
+                                       "deviceToken" : self.appPreference.getDeviceToken(),
+                                       "loginType": kLoginTypeFacebook,
+                                       "profile_image":"http://graph.facebook.com/\(fbData["id"] ?? "")/picture?type=large",
+                                       "socialId":"\(fbData["id"] ?? "")",
+                                       "name":"\(fbData["name"] ?? "")",
+                                       "model": UIDevice.current.model,
+                                       "version": UIDevice.current.systemVersion
+                                   ]
+
+                                   print("param facebook... \(param)")
+
+                                   self.loginWithSocial(param: param as Dictionary<String, Any>)
+                               }else {
+                                   let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMSignupEmailVC") as! WWMSignupEmailVC
+                                   vc.isFromFb = true
+                                   vc.fbData = fbData
+                                   self.navigationController?.pushViewController(vc, animated: true)
+                               }
+                           }
+                       }else {
+                           print(error?.localizedDescription ?? "")
+                       }
+                   })
+                   }
+               }
+           }
+       }
+       
+       @IBAction func btnLoginWithGoogleAction(_ sender: UIButton) {
+           WWMHelperClass.sendEventAnalytics(contentType: "SIGN_IN", itemId: "GOOGLE", itemName: "")
+            GIDSignIn.sharedInstance()?.delegate = self
+           GIDSignIn.sharedInstance()?.uiDelegate = self
+           GIDSignIn.sharedInstance()?.signIn()
+       }
+       
+       @IBAction func btnPrivacyPolicyAction(_ sender: Any) {
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMWebViewVC") as! WWMWebViewVC
+               vc.strUrl = URL_PrivacyPolicy
+               vc.strType = "Privacy Policy"
+           self.navigationController?.pushViewController(vc, animated: true)
+       }
+       
+       @IBAction func btnTermsOfUseAction(_ sender: Any) {
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMWebViewVC") as! WWMWebViewVC
+           vc.strUrl = URL_TermsnCondition
+           vc.strType = "Terms & Conditions"
+           self.navigationController?.pushViewController(vc, animated: true)
+       }
+       
+       
+       // Google Login Delegate Method
+       
+       func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+                 withError error: Error!) {
+           if let error = error {
+               print("\(error.localizedDescription)")
+           } else {
+               // Perform any operations on signed in user here.
+               let userId = user.userID                  // For client-side use only!
+              // let idToken = user.authentication.idToken // Safe to send to the server
+               let fullName = user.profile.name
+              // let givenName = user.profile.givenName
+              // let familyName = user.profile.familyName
+               let email = user.profile.email
+               let profileImage = user.profile.imageURL(withDimension: 400)
+               let param = [
+                   "email": email,
+                   "password":"",
+                   "deviceId": kDeviceID,
+                   "DeviceType": kDeviceType,
+                   "deviceToken" : self.appPreference.getDeviceToken(),
+                   "loginType": kLoginTypeGoogle,
+                   "profile_image":profileImage?.absoluteString,
+                   "socialId":userId,
+                   "name":fullName,
+                   "model": UIDevice.current.model,
+                   "version": UIDevice.current.systemVersion
+               ]
+            
+               self.loginWithSocial(param: param as Dictionary<String, Any>)
+               
+           }
+       }
+    
+    func loginWithSocial(param:[String : Any]) {
+        self.view.endEditing(true)
+        //WWMHelperClass.showSVHud()
+        WWMHelperClass.showLoaderAnimate(on: self.view)
+        
+        
+        let param = [
+            "email": txtViewEmail.text ?? "",
+            "password":"",
+            "deviceId": kDeviceID,
+            "deviceToken" : appPreference.getDeviceToken(),
+            "DeviceType": kDeviceType,
+            "loginType": kLoginTypeFacebook,
+            "profile_image":"http://graph.facebook.com/\(fbData["id"] ?? "")/picture?type=large",
+            "socialId":"\(fbData["id"] ?? -1)",
+            "name":"\(fbData["name"] ?? "")",
+            "model": UIDevice.current.model,
+            "version": UIDevice.current.systemVersion
+            ] as [String : Any]
+        
+        print("param2... \(param)")
+        WWMWebServices.requestAPIWithBody(param:param , urlString: URL_LOGIN, context: "WWMSignupEmailVC", headerType: kPOSTHeader, isUserToken: false) { (result, error, sucess) in
+            if sucess {
+                
+                if let userProfile = result["userprofile"] as? [String:Any] {
+                    
+                    print("userProfile WWMSignupEmailVC... \(userProfile)")
+                    
+                    if let isProfileCompleted = userProfile["IsProfileCompleted"] as? Bool {
+                        self.appPreference.setIsLogin(value: true)
+                        self.appPreference.setUserID(value:"\(userProfile["user_id"] as? Int ?? 0)")
+                        //Crashlytics.sharedInstance().setUserIdentifier("userId \(userProfile["user_id"] as? Int ?? 0)")
+                        
+                        Crashlytics.crashlytics().setUserID("userId \(userProfile["user_id"] as? Int ?? 0)")
+                        
+                        //NotificationCenter.default.post(name: Notification.Name(rawValue: "logoutSuccessful"), object: nil)
+                        
+                        self.appPreference.setEmail(value: userProfile["email"] as? String ?? "")
+                        self.appPreference.setUserToken(value: userProfile["token"] as? String ?? "")
+                        self.appPreference.setUserName(value: userProfile["name"] as? String ?? "")
+                        self.appPreference.setIsProfileCompleted(value: isProfileCompleted)
+                        self.appPreference.setType(value: userProfile["type"] as? String ?? "")
+                        self.appPreference.setGuideType(value: userProfile["guided_type"] as? String ?? "")
+                        self.appPreference.setGuideTypeFor3DTouch(value: userProfile["guided_type"] as? String ?? "")
+                        self.appPreference.setUserData(value: [:])
+                        
+                        print("self.appPreference.getUserName() ...... \(self.appPreference.getUserName())")
+                        
+                        self.appPreference.setHomePageURL(value: userProfile["home_page_url"] as! String)
+                        self.appPreference.setLearnPageURL(value: userProfile["learn_page_url"] as! String)
+                        
+                        if isProfileCompleted {
+                            
+                            self.appPreference.setGetProfile(value: true)
+                            
+                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMTabBarVC") as! WWMTabBarVC
+                            UIApplication.shared.keyWindow?.rootViewController = vc
+                            
+                            // UIApplication.shared.keyWindow?.rootViewController = AppDelegate.sharedDelegate().animatedTabBarController()
+                        }else {
+                            
+                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "WWMWalkThoghVC") as! WWMWalkThoghVC
+                            
+                            vc.value = "SignupLetsStart"
+                            self.navigationController?.pushViewController(vc, animated: true)
+                        }
+                    }
+                }
             }else {
                 if error?.localizedDescription == "The Internet connection appears to be offline."{
                     WWMHelperClass.showPopupAlertController(sender: self, message: internetConnectionLostMsg, title: kAlertTitle)
