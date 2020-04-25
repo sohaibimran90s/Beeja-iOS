d0a554fcb4939a462b13cc69f37b913dde655c5c
prema negi <prema.negi@iris-worldwide.com>
2020-04-25 12:30:39 +0530

    stringDefine false

--- Meditation/Helper/WWMStringDefine.swift
+++ Meditation/Helper/WWMStringDefine.swift
@@ -339,7 +339,7 @@ let kNO = "NO"
 
 
 // If kBETA_ENABLED is true its mean we are using beta_url else using staging_url
-let kBETA_ENABLED = true
+let kBETA_ENABLED = false
 
 /*dynamic webView URLs
  let URL_PrivacyPolicy   =  URL_BASE_WEBVIEW + "/privacy-policy?mobile_view=1"
