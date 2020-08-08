28c2cbcdfaa0f85e932aef2903b932bc04d56791
Ehsanul Haque <ehsanul.haque@iris-worldwide.com>
2020-08-08 19:51:51 +0530

    Font Corrected for Journal Text and Image entry with underline.

--- Meditation/ViewControllers/Journal/Utils/Constant.swift
+++ Meditation/ViewControllers/Journal/Utils/Constant.swift
@@ -14,12 +14,13 @@ class Constant: NSObject {
     static let DEFAULT_CONTENT_TYPE = "application/json; charset=UTF-8";
     
     static let Light_Green = UIColor(red: 94/255, green: 236/255, blue: 154/255, alpha: 1.0)
-    
+    static let Light_Green_Underline =  UIColor.init(red: 0.0/255.0, green: 235.0/255.0, blue: 169.0/255.0, alpha: 1.0)
+
     static let underLineAttrs = [
-    NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16.0),
+    //NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16.0), // Maxx-medium set from XIB
     NSAttributedString.Key.foregroundColor : UIColor.white,
-    NSAttributedString.Key.underlineColor:Light_Green,
-    NSAttributedString.Key.underlineStyle : 1] as [NSAttributedString.Key : Any]
+    NSAttributedString.Key.underlineColor:Light_Green_Underline,
+    NSAttributedString.Key.underlineStyle : NSUnderlineStyle.single.rawValue] as [NSAttributedString.Key : Any]
     
     
     static let FILENAME = "myRecording.m4a"
